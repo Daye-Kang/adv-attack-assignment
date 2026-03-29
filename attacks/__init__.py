7c8ca1d0531768d0631d3dcb406ebb565a994720
brain-ai-create <dayekang012@gmail.com>
2026-03-29 18:45:55 +0900

    Fix function name

--- attacks/__init__.py
+++ attacks/__init__.py
@@ -1,4 +1,4 @@
-from .fgsm import fgsm_attack
-from .pgd import pgd_attack
+from .fgsm import fgsm_targeted, fgsm_untargeted
+from .pgd import pgd_targeted, pgd_untargeted
 
-__all__ = ["fgsm_attack", "pgd_attack"]
+__all__ = ["fgsm_targeted", "fgsm_untargeted", "pgd_targeted", "pgd_untargeted"]
